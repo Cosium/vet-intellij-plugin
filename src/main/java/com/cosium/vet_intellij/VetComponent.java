ffd02059b240e4c0211167c619a28c40718be046
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2018-05-24 18:32:34 +0200

    Release 1.4

--- src/main/java/com/cosium/vet_intellij/VetComponent.java
+++ src/main/java/com/cosium/vet_intellij/VetComponent.java
@@ -22,7 +22,7 @@ public class VetComponent extends Vet implements ProjectComponent {
   public static final String DISPLAY_ID = "Vet";
 
   static {
-    VetVersion.setValue("1.3_intellij-plugin");
+    VetVersion.setValue("1.4_intellij-plugin");
   }
 
   public VetComponent(Project project) {
